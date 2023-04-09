60266ae8a4ddd1317468e83581a5514a7a194e5f
Daniel Castro Martin <109592821+DCM91@users.noreply.github.com>
2023-04-09 13:46:17 +0200

    comit antes de provar seversideprops

--- components/header.tsx
+++ components/header.tsx
@@ -2,10 +2,8 @@ import React from 'react'
 
 export default function HeaderLanding() {
   return (
-    <>
-      <div className='scale-y-75'>
-       <img src="https://www.viva-media.ca/wp-content/uploads/header-culture-cinema.jpg" alt="" className=" w-full h-85"/>
-      </div>
-    </>
+     <div className='scale-y-75'>
+     <img src="https://www.viva-media.ca/wp-content/uploads/header-culture-cinema.jpg" alt="" className=" w-full h-auto"/>
+    </div> 
   )
 }
--- pages/[films].tsx
+++ pages/[films].tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function pelis() {
   return (
-    <Footer>
+    <Footer title='films'>
       <div className="h-96 bg-red-600">
         eoooooooooooooooooooooo
       </div>
